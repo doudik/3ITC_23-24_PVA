7ed221b1c8fd500d85aff05032743ec5d8951090
Miroslav Douda <miroslavdouda@spsul.cz>
2023-12-13 10:29:31 +0100

    ?

--- ManagementApp - Composite/ManagementApp - Composite/Program.cs
+++ ManagementApp - Composite/ManagementApp - Composite/Program.cs
@@ -9,16 +9,17 @@
             Departments economy = new Departments("Economy");
             Departments IT = new Departments("IT");
 
-
             IT.Add(new Employee("Karel", "Kovář", IT));
             IT.Add(new Employee("Franta", "Františků", IT));
             IT.Add(new Employee("Michal", "Procházka", IT));
             IT.Add(new Employee("Petr", "Černý", IT));
+
             management.Add(new Employee("Pavel", "Šotola", management));
             management.Add(new Employee("Antonín", "Svobodka", management));
             management.Add(new Employee("Svatopluk", "Němec", management));
             management.Add(new Employee("Vratislav", "Douda", management));
             management.Add(new Employee("František", "The Este", management));
+
             economy.Add(new Employee("Ludmila", "Černá", economy));
             economy.Add(new Employee("Miloslava", "Doudová", economy));
             economy.Add(new Employee("Matěj", "The Skůtr", economy));
